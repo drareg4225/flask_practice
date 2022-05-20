30aec96abce92eb5cc31934ab5f799ae1cc250d7
drareg4225 <drareg_ge4225@yahoo.com>
2022-05-20 11:51:55 -0700

    First Commit - b4 Jinja Loops

---
+++ flashcards.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template , abort
+from model import db
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return render_template("welcome.html", message = "Here's a message from the view.")
+
+
+@app.route("/card/<int:index>")
+def card_view(index):
+    try:
+        card = db[index]
+        return render_template("card.html", 
+                                card = card , 
+                                index=index ,
+                                max_index= len(db) -1 )
+    except IndexError:
+        abort(404)
